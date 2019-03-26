ca11b5c23d638057199da8d4d3e5f5ace819b092
Alexandre Juca <corextechnologies@gmail.com>
2019-03-26 11:59:26 +0100

    Fix issue that causes compile error when targeting API below 23 in projects using this library

--- app/src/main/java/io/github/alexjuca/kiyoSample/MainActivity.java
+++ app/src/main/java/io/github/alexjuca/kiyoSample/MainActivity.java
@@ -1,17 +1,17 @@
-package io.github.alexjuca.kiyo;
+package io.github.alexjuca.kiyoSample;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import io.github.alexjuca.kiyo.Kiyo;
+import io.github.alexjuca.kiyo.KiyoListener;
 
 public class MainActivity extends AppCompatActivity {
     private KiyoListener kiyoListener = new KiyoListener() {
